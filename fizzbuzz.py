5f4608dc02621859edc5c64fda312aa2a2bdb029
Shota Narisaka <shootacean@gmail.com>
2023-06-14 21:25:56 +0900

    feat(fizzbuzz): add fizzbuzz with github copilot

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n):
+    if n % 15 == 0:
+        return 'FizzBuzz'
+    elif n % 5 == 0:
+        return 'Buzz'
+    elif n % 3 == 0:
+        return 'Fizz'
+    else:
+        return str(n)
+    
+if __name__ == '__main__':
+    for i in range(1, 101):
+        print(fizzbuzz(i))
