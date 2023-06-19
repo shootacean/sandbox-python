0cdb7489aa7fbd6ff17eb2ecdf1d13d3949c1205
Shota Narisaka <shootacean@gmail.com>
2023-06-19 22:42:25 +0900

    style(multiprocess): run formatter and fix

--- multiprocess/src/main.py
+++ multiprocess/src/main.py
@@ -1,9 +1,10 @@
 # https://docs.python.org/ja/3/library/multiprocessing.html
 from multiprocessing import Pool
 
+
 def f(x):
     return x*x
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with Pool(5) as p:
         print(p.map(f, [1,2,3]))
